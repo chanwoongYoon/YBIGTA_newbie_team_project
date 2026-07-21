4891d37ba66bbd1e5ba4d16df34d94c6e961c1e1
chanwoongYoon <cwyoon9718@gmail.com>
2026-07-21 10:15:08 +0900

    폴더구조 원상복구

---
+++ app/user/user_router.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, HTTPException, Depends, status
+from app.user.user_schema import User, UserLogin, UserUpdate, UserDeleteRequest
+from app.user.user_service import UserService
+from app.dependencies import get_user_service
+from app.responses.base_response import BaseResponse
+
+user = APIRouter(prefix="/api/user")
+
+
+@user.post("/login", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
+def login_user(user_login: UserLogin, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    try:
+        user = service.login(user_login)
+        return BaseResponse(status="success", data=user, message="Login Success.") 
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
+def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    try:
+        registered_user = service.register_user(user)
+        return BaseResponse(status="success", data=registered_user, message="User registration success.")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
+def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    try:
+        deleted_user = service.delete_user(user_delete_request.email)
+        return BaseResponse(status="success", data=deleted_user, message="User Deletion Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+
+@user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
+def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
+    try:
+        updated_user = service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=updated_user, message="User password update success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
