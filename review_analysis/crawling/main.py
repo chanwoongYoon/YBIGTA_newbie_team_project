91781a4d981e1333e89ba8cea15d8aea760e55f5
chanwoongYoon <cwyoon9718@gmail.com>
2026-07-21 11:26:30 +0900

    add : 크롤링 클래스 main에 명시

--- review_analysis/crawling/main.py
+++ review_analysis/crawling/main.py
@@ -2,10 +2,12 @@ from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.crawling.base_crawler import BaseCrawler
 from review_analysis.crawling.example_crawler import ExampleCrawler
+from review_analysis.crawling.rotten_tomato_crawler import RottenTomatoCrawler
 
-# 모든 크롤링 클래스를 예시 형식으로 적어주세요. 
+# 모든 크롤링 클래스를 예시 형식으로 적어주세요.
 CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
     "example": ExampleCrawler,
+    "rotten_tomato": RottenTomatoCrawler,
 }
 
 def create_parser() -> ArgumentParser:
